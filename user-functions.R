963ddf7b01df0a119a3afe190673117e20bef596
cseanburns <sean.csb@gmail.com>
2020-03-08 15:40:35 -0400

    added column

--- user-functions.R
+++ user-functions.R
@@ -1,7 +1,9 @@
 # Function to return specific counts for database and set
 # Usage example: retrieveCounts("OML", "set11")
 retrieveCounts <- function(db, s) {  
-cbind(lqueries$database[lqueries$database == db & lqueries$Set == s],
-      lqueries$counts[lqueries$database == db & lqueries$Set == s],
-      as.character(lqueries$Set[lqueries$database == db & lqueries$Set == s]))
+cbind(wqueries$database[wqueries$database == db & wqueries$Set == s],
+      wqueries$counts[wqueries$database == db & wqueries$Set == s],
+      as.character(wqueries$months[wqueries$database == db & wqueries$Set == s]),
+      as.character(wqueries$Set[wqueries$database == db & wqueries$Set == s]))
 }
+
