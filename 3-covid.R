5c8e94b12dc5c0bb68cb3d6d2cc323f21f1e5f1b
C. Sean Burns <sean.csb@gmail.com>
2020-08-14 13:17:41 -0400

    fixed plot lines and colors

--- 3-covid.R
+++ 3-covid.R
@@ -6,16 +6,16 @@
 # 
 # This script generates Figure 9.
 
-
+rm(list = ls())
 covidqueries <- read.csv("data/covid-pubmed-searches-long.csv", sep = ",")
 covidqueries$Date <- as.Date(covidqueries$Date, "%Y-%m-%d")
 
 tiff('plots/fig9-covid-searches.tif', width = 3840, height = 2160, pointsize = 12, res = 300)
 plot(covidqueries$Date[covidqueries$Databases=="PubMedLegacyMostRecent"],
      covidqueries$Hits[covidqueries$Databases=="PubMedLegacyMostRecent"],
-     type = "b",
-     pch = 19,
-     col = "red",
+     type = "o",
+     pch = 18,
+     col = "#999999",
      xlab = "Dates",
      ylab = "Search Hits",
      xlim = range(covidqueries$Date),
@@ -24,41 +24,42 @@ plot(covidqueries$Date[covidqueries$Databases=="PubMedLegacyMostRecent"],
 lines(covidqueries$Date[covidqueries$Databases=="PubMedLegacyBestMatch"],
       covidqueries$Hits[covidqueries$Databases=="PubMedLegacyBestMatch"],
       pch = 19,
-      col = "orange",
-      type = "b")
+      col = "#E69F00",
+      type = "o")
 
 lines(covidqueries$Date[covidqueries$Databases=="PubMedNew"],
       covidqueries$Hits[covidqueries$Databases=="PubMedNew"],
-      pch = 19,
-      col = "black",
-      type = "b")
+      pch = 20,
+      col = "#56B4E9",
+      type = "o")
 
 lines(covidqueries$Date[covidqueries$Databases=="ProQuestPubmed"],
       covidqueries$Hits[covidqueries$Databases=="ProQuestPubmed"],
-      pch = 19,
-      col = "blue",
-      type = "b")
+      pch = 21,
+      col = "#009E73",
+      type = "o")
 
 lines(covidqueries$Date[covidqueries$Databases=="EBSCOhostPubMed"],
       covidqueries$Hits[covidqueries$Databases=="EBSCOhostPubMed"],
-      pch = 19,
-      col = "green",
-      type = "b")
+      pch = 22,
+      col = "#F0E442",
+      type = "o")
 
 lines(covidqueries$Date[covidqueries$Databases=="WebofSciencePubMed"],
       covidqueries$Hits[covidqueries$Databases=="WebofSciencePubMed"],
-      pch = 19,
-      col = "gray",
-      type = "b")
+      pch = 23,
+      col = "#0072B2",
+      type = "o")
  
 lines(covidqueries$Date[covidqueries$Databases=="OvidPubMed"],
       covidqueries$Hits[covidqueries$Databases=="OvidPubMed"],
-      pch = 19,
-      col = "purple",
-      type = "b")
+      pch = 24,
+      col = "#D55E00",
+      type = "o")
 
 legend("topleft", legend=c("PMLMR", "PMLBM", "PMN",
                        "PrQPM", "EPM", "WSPM", "OPM"),
-       col = c("red", "orange", "black", "blue", "green", "gray", "purple"),
-       lty=1:5, cex=0.8, ncol = 1)
+       col = c("#999999", "#E69F00", "#56B4E9", "#009E73",
+               "#F0E442", "#0072B2", "#D55E00"),
+       lty=1, cex=0.8, ncol = 1, bty = "n")
 dev.off()
